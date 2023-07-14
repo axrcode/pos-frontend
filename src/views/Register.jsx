facb1fea7734d404e914d5e0a62129b850b6265b
axrcode <axrcode@gmail.com>
2023-07-13 22:12:57 -0600

    Add: Formularios para login y registro

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,6 +1,92 @@
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <>
+      <h1 className="text-4xl font-black">Crea tu Cuenta</h1>
+      <p>Crea tu cuenta llenando el formulario</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-7 py-10">
+        <form>
+
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="text-slate-800 font-bold"
+            >Nombre:</label>
+
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Tu nombre"
+              className="mt-2 w-full p-3 bg-gray-50 border-solid border-2 border-gray-200 rounded-md focus:outline-none focus:ring"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="text-slate-800 font-bold"
+            >Email:</label>
+
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Tu email"
+              className="mt-2 w-full p-3 bg-gray-50 border-solid border-2 border-gray-200 rounded-md focus:outline-none focus:ring"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="text-slate-800 font-bold"
+            >Contraseña:</label>
+
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu contraseña"
+              className="mt-2 w-full p-3 bg-gray-50 border-solid border-2 border-gray-200 rounded-md focus:outline-none focus:ring"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="password_confirm"
+              className="text-slate-800 font-bold"
+            >Confirmar Contraseña:</label>
+
+            <input
+              type="password"
+              id="password_confirm"
+              name="password_confirm"
+              placeholder="Confirmar contraseña"
+              className="mt-2 w-full p-3 bg-gray-50 border-solid border-2 border-gray-200 rounded-md focus:outline-none focus:ring"
+              autoComplete="off"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer rounded-md"
+          />
+
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        <Link to="/auth/login">
+          ¿Ya tienes cuenta? Inicia sesión
+        </Link>
+      </nav>
+    </>
   )
 }
